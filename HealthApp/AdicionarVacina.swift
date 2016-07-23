dcf97b1f12bf9932f99a159caded3e1ca693f085
arthurbfcarvalho <arthurbfcarvalho@gmail.com>
2016-07-23 14:58:43 -0300

    comentarios

--- HealthApp/AdicionarVacina.swift
+++ HealthApp/AdicionarVacina.swift
@@ -18,12 +18,22 @@ class AdicionarVacina: UIViewController {
     @IBOutlet var textField2: UITextField!
     @IBOutlet var textField3: UITextField!
     @IBOutlet var textField4: UITextField!
+     override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textFieldStyles(textField1)
+        textFieldStyles(textField2)
+        textFieldStyles(textField3)
+        textFieldStyles(textField4)
+
+    }
+
     
-    var myArray: [String] = [""]
+    // a partir daqui: ****KEYBOARD****
     
-  
     
     // Movimentar a View quando o Keyboard surgir
+    
     var keyboardHeight: CGFloat!
     
     func animateTextField(up: Bool) {
@@ -70,44 +80,5 @@ class AdicionarVacina: UIViewController {
         textField.leftViewMode = UITextFieldViewMode.Always
         textField.leftView = SpacerView
     }
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        textFieldStyles(textField1)
-        textFieldStyles(textField2)
-        textFieldStyles(textField3)
-        textFieldStyles(textField4
-        
-        )
-        
-        
-        
-//        //informações referentes à
-//        textField1.layer.shadowColor = UIColor.blackColor().CGColor
-//        textField1.layer.shadowRadius = 1
-//        textField1.layer.shadowOffset = CGSizeMake(0, 1)
-//        textField1.layer.shadowOpacity = 0.3
-//        textField1.clipsToBounds = false
-//        
-//        textField2.layer.shadowColor = UIColor.blackColor().CGColor
-//        textField2.layer.shadowRadius = 1
-//        textField2.layer.shadowOffset = CGSizeMake(0, 1)
-//        textField2.layer.shadowOpacity = 0.3
-//        textField2.clipsToBounds = false
-//        
-//        textField3.layer.shadowColor = UIColor.blackColor().CGColor
-//        textField3.layer.shadowRadius = 1
-//        textField3.layer.shadowOffset = CGSizeMake(0, 1)
-//        textField3.layer.shadowOpacity = 0.3
-//        textField3.clipsToBounds = false
-//        
-//        textField4.layer.shadowColor = UIColor.blackColor().CGColor
-//        textField4.layer.shadowRadius = 1
-//        textField4.layer.shadowOffset = CGSizeMake(0, 1)
-//        textField4.layer.shadowOpacity = 0.3
-//        textField4.clipsToBounds = false
-        
-    }
+    
 }
